6537339c62d55c801b16148582fb159ad71712d1
Lukeslux <lucas.brnd@gmail.com>
2017-01-20 13:42:34 +0100

    Added Simple credits menu.

---
+++ Assets/Scripts/GUI/Actions/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Simple class that loads the scene of the given index. */
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadSceneAsync(buildIndex, LoadSceneMode.Single);
+    }
+}
--- Assets/Scripts/GUI/Effects/Jiggle.cs
+++ Assets/Scripts/GUI/Effects/Jiggle.cs
@@ -8,10 +8,12 @@ public class Jiggle : MonoBehaviour {
     public float speedY = 10;
     public float amplitudeX = 10;
     public float amplitudeY = 10;
+    public float phaseX = 0;
+    public float phaseY = 0;
     private RectTransform rectTransform;
     private Vector3 origin;
-    private float phaseX = 0;
-    private float phaseY = 0;
+    private float omegaX = 0;
+    private float omegaY = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -21,10 +23,12 @@ public class Jiggle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.phaseX += Time.deltaTime * speedX;
-        this.phaseY += Time.deltaTime * speedY;
-        float moveX =  amplitudeX * Mathf.Cos(phaseX);
-        float moveY = amplitudeY * Mathf.Sin(phaseY);
+        this.omegaX += Time.deltaTime * speedX;
+        this.omegaY += Time.deltaTime * speedY;
+        this.omegaX %= Mathf.PI * 2;
+        this.omegaY %= Mathf.PI * 2;
+        float moveX =  amplitudeX * Mathf.Cos(omegaX+phaseX);
+        float moveY = amplitudeY * Mathf.Sin(omegaY+phaseY);
         rectTransform.localPosition = origin + new Vector3(moveX, moveY);
 	}
 }
