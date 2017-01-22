4c72d8196a43e308cf83e590238080cbfc799f3e
Lukeslux <lucas.brnd@gmail.com>
2017-01-22 12:43:00 +0100

    Start button only shown when there are zero players.

---
+++ Assets/Scripts/GUI/Effects/OnlyWithPlayers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasRenderer))]
+public class OnlyWithPlayers : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        int playerCount = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (playerCount == 0)
+            gameObject.GetComponent<CanvasRenderer>().SetAlpha(0); 
+        else
+            gameObject.GetComponent<CanvasRenderer>().SetAlpha(1);
+    }
+}
