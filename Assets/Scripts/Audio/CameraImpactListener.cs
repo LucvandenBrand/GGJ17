a94e37446750b6f38051cfe8319be2d1047c60ed
Timo Strating <timostrating@home.nl>
2017-01-22 05:03:10 +0100

    chroma support NOT SETUP JET

---
+++ Assets/Scripts/Audio/CameraImpactListener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraImpactListener : AudioImpactListener {
+
+
+
+    public override void AudioImpact( float reguestedCallbackValue ) {
+        Debug.Log( "AudioImpact : " + reguestedCallbackValue );
+        //Camera.main.orthographicSize = reguestedCallbackValue;
+    }
+
+    
+    public void Start() {
+        AudioSystemController.GetAudioSystemController().AddAudioImpactListener( this, AudioImpactType.BASE_INTENSITY);
+    }
+}
