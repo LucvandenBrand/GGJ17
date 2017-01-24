966097428c1e0a453f207e0fbe4a76c400dbcb98
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2017-01-24 18:22:39 +0100

    WIP changing SelectAudioFile to work again on Linux

--- Assets/Scripts/Audio/SelectAudioFile.cs
+++ Assets/Scripts/Audio/SelectAudioFile.cs
@@ -35,27 +35,91 @@ public class SelectAudioFile : MonoBehaviour {
         StartCoroutine(LoadAudio(audioPath));
     }
 
+    /*
+      Only used on Linux right now.
+      */
+    private string SoxFilename()
+    {
+        #if UNITY_EDITOR_LINUX
+            return Path.Combine("sox", "sox_linux");
+        #elif UNITY_EDITOR_WINDOWS
+            return Path.Combine("sox", "sox.exe");
+        #elif UNITY_STANDALONE_LINUX
+            return Path.Combine("EyeCantHear_Data", Path.Combine("sox","sox_linux"));
+        #else
+            return Path.Combine("sox", "sox.exe");
+        #endif
+
+    }
+
+    /*
+      Only used on Linux right now.
+    */
+    private string convertSoundToWav(string audioPath)
+    {
+        string tmpWavPath =Path.Combine(Application.dataPath, "tmp.wav");
+
+        Process process = new Process();
+        process.StartInfo.FileName = SoxFilename();
+        process.StartInfo.WorkingDirectory = Application.dataPath;
+        process.StartInfo.Arguments = "\"" + audioPath + "\" " + tmpWavPath;
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.Start();
+
+        //UnityEngine.Debug.Log("Starting Done");
+
+        var standardOutput = new System.Text.StringBuilder();
+        var standardError = new System.Text.StringBuilder();
+
+        // read chunk-wise while process is running.
+        while (!process.HasExited)
+        {
+            standardOutput.Append(process.StandardOutput.ReadToEnd());
+            standardError.Append(process.StandardError.ReadToEnd());
+        }
+
+        // make sure not to miss out on any remaindings.
+        standardOutput.Append(process.StandardOutput.ReadToEnd());
+        standardError.Append(process.StandardError.ReadToEnd());
+
+
+        //process.WaitForExit();
+        //UnityEngine.Debug.Log(standardOutput);
+        if (standardError.ToString() != "")
+        {
+            UnityEngine.Debug.Log(standardError);
+        }
+        return tmpWavPath;
+    }
+
+
 
 
     IEnumerator LoadAudio( string audioPath ) {
+
+        #if UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
+            if (!audioPath.EndsWith(".wav"))
+            {
+                audioPath = convertSoundToWav(audioPath);
+            }
+        #endif
+
         string url = "file:///" + audioPath;
         WWW www = new WWW(url);
         while (!www.isDone) {
             yield return new WaitForEndOfFrame();
         }
 
-
-        // resultClip = (audioPath.EndsWith(".mp3")) ? NAudioPlayer.FromMp3Data(www.bytes) : www.audioClip;
-        audioSource.clip = resultClip = NAudioPlayer.FromMp3Data( www.bytes ); //resultClip;
+        #if UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
+            audioSource.clip = resultClip = www.audioClip;
+        #else
+            // resultClip = (audioPath.EndsWith(".mp3")) ? NAudioPlayer.FromMp3Data(www.bytes) : www.audioClip;
+            audioSource.clip = resultClip = NAudioPlayer.FromMp3Data( www.bytes ); //resultClip;
+        #endif
         yield return null;
-    }
-
-
-    //deze wegdoen als er muziek niet meer automatisch gestart moet worden
-    void Update() {
-        if (audioSource.clip != null) {
-            if (!audioSource.isPlaying && audioSource.clip.isReadyToPlay)
-                audioSource.Play();
-        }
     }
 }
